fed93a721106cb3ce66eff49bc2d6cbffbd843f1
Nicolas Vera <nico03veraa@gmail.com>
2023-05-13 01:48:12 -0300

    Se agregaron las politicas de CORS

--- src/main/java/com/portfolio/njvp/Controller/Controller.java
+++ src/main/java/com/portfolio/njvp/Controller/Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-4bf0a.web.app")
 public class Controller {
 
     @Autowired
--- src/main/java/com/portfolio/njvp/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/njvp/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = {"https://portfolio-frontend-4bf0a.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
